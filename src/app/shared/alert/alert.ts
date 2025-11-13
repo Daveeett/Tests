bb353a50303c448fcc04664aa5fe6c0f986c58f5
David <davidalbertotumbaco@gmail.com>
2025-11-13 10:28:25 -0500

    fixing bugs and add some bugs

---
+++ src/app/shared/alert/alert.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  imports: [],
+  templateUrl: './alert.html',
+  styleUrl: './alert.css',
+})
+export class Alert {
+  message!: string;
+
+  constructor(){}
+
+  public getContent(){
+
+
+  }
+
+}
